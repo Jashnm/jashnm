41f95b6129a9f5ef99d8dd6da89320654b71b0ef
Jashn <itsjashn@live.com>
2021-11-05 00:28:36 +0530

    changed commit count process to give more precise result

--- index.js
+++ index.js
@@ -2,40 +2,36 @@ import { promises as fs } from "fs";
 import path from "path";
 import fetch from "node-fetch";
 
+const year = new Date().getFullYear();
+
 const main = async () => {
   const template = await (
     await fs.readFile(path.join(process.cwd(), "/README.template.md"))
   ).toString("utf-8");
 
   const getGhCommits = async () => {
-    const year = new Date().getFullYear();
     try {
-      const res = await fetch(
-        `https://api.github.com/search/commits?q=author:jashnm+author-date:>${
-          year - 1
-        }`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/vnd.github.cloak-preview",
-            Authorization: `bearer ${process.env.JASHN_GH_PCT}`
-          }
+      const response = await fetch(`https://api.github.com/user/emails`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/vnd.github.cloak-preview",
+          Authorization: `bearer ${process.env.JASHN_GH_PCT}`
         }
+      });
+      let emailsList = await response.json();
+      const commits = await Promise.all(
+        emailsList.map((x) => getCommitsByEmail(x.email))
       );
 
-      const myCommits = await res.json();
+      const commitCount = commits.reduce((acc, curr) => acc + curr);
 
       let committedTreesText = `Committed trees(${year}): `;
-
-      if (myCommits && myCommits.total_count) {
+      if (!!commitCount) {
         const readme = template.replace(
           "{committed_trees}",
-          `${committedTreesText}${myCommits.total_count}  \n ${Array(
-            myCommits.total_count
-          )
-            .fill(null)
-            .map(() => "🌳")
+          `${committedTreesText}${commitCount}  \n ${Array(commitCount)
+            .fill("🌳")
             .join("")}`
         );
 
@@ -44,10 +40,32 @@ const main = async () => {
     } catch (err) {
       console.log(err);
     }
-
-    return;
   };
   getGhCommits();
+};
+
+const getCommitsByEmail = async (email) => {
+  try {
+    const res = await fetch(
+      `https://api.github.com/search/commits?q=author-email:${email}+author-date:>${
+        year - 1
+      }`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/vnd.github.cloak-preview",
+          Authorization: `bearer ${process.env.JASHN_GH_PCT}`
+        }
+      }
+    );
+
+    const myCommits = await res.json();
+
+    return myCommits.total_count;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 main();
